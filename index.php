ae0c017449698746af35c2fea7aa848768bd28b5
Harshil Shishangiya <harshilshishangiya777@gmail.com>
2025-11-14 23:00:21 +0530

    update index file

---
+++ index.php
@@ -0,0 +1,60 @@
+echo "<h2>budget_app Harshil</h2>";
+echo "<ul>";
+
+echo "<h3>User API</h3>";
+
+echo "<li><a href='budget_app/ViewUsers.php'></a>View User</li>";
+echo"<li><a href='budget_app/SignUp.php'></a>Sign Up</li>";
+echo"<li><a href='budget_app/Profile.php'></a>Profile View</li>";
+echo"<li><a href='budget_app/ProfileUpdate.php'></a>Profile Update</li>";
+echo"<li><a href='budget_app/ProfileDelete.php'></a>Profile Delete</li>";
+
+echo "<h3>Account API</h3>";
+
+echo"<li><a href='budget_app/ViewAccounts.php'></a>View Accounts</li>";
+echo"<li><a href='budget_app/AddAccount.php'></a>Add Account</li>";
+echo"<li><a href='budget_app/UpdateAccount.php'></a>Update Account</li>";
+echo"<li><a href='budget_app/DeleteAccount.php'></a>Delete Account</li>";
+echo"<li><a href='budget_app/DeleteAccount.php'></a>Delete Account</li>";
+
+echo "<h3>Transaction</h3>";
+
+echo "<li><a href='budget_app/AddTransaction.php'></a>Add Transaction</li>";
+echo "<li><a href='budget_app/ViewTransactions.php'></a>View Transactions</li>";
+echo "<li><a href='budget_app/UpdateTransaction.php'></a>Update Transaction</li>";
+echo "<li><a href='budget_app/DeleteTransaction.php'></a>Delete Transaction</li>";
+echo "<li><a href='budget_app/SummaryReportTransaction.php'></a>Summary Report Transaction</li>";
+
+echo "<h3>Category</h3>";
+
+echo "<li><a href='budget_app/AddCategory.php'></a>Add Category</li>";
+echo "<li><a href='budget_app/ViewCategory.php'></a>View Category</li>";
+echo "<li><a href='budget_app/UpdateCategory.php'></a>Update Category</li>";
+echo "<li><a href='budget_app/DeleteCategory.php'></a>Delete Category</li>";
+
+echo "<h3>Expense</h3>";
+
+echo "<li><a href='budget_app/AddExpense.php'></a>Add Expense</li>";
+echo "<li><a href='budget_app/ViewExpenses.php'></a>View Expenses</li>";
+echo "<li><a href='budget_app/DeleteExpense.php'></a>Delete Expense</li>";
+echo "<li><a href='budget_app/UpdateExpense.php'></a>Update Expense</li>";
+echo "<li><a href='budget_app/ViewExpenseById.php'></a>View Expense By Id</li>";
+
+
+echo "<h3>Income</h3>";
+
+echo "<li><a href='budget_app/AddIncome.php'></a>Add Income</li>";
+echo "<li><a href='budget_app/ViewIncome.php'></a>View Income</li>";
+echo "<li><a href='budget_app/UpdateIncome.php'></a>Update Income</li>";
+echo "<li><a href='budget_app/DeleteIncome.php'></a>Delete Income</li>";
+echo "<li><a href='budget_app/ViewIncomeById.php'></a>View Income By Id</li>";
+
+
+echo "<h3>Total</h3>";
+
+echo "<li><a href='budget_app/SummaryReport.php'></a>Summary Report</li>";
+
+
+echo "</ul>";
+
+
