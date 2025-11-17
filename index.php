dbb01c16e94cca2019b6207486b7edad809f5269
Harshil Shishangiya <harshilshishangiya777@gmail.com>
2025-11-17 20:43:48 +0530

    Update index.php
    some change

--- index.php
+++ index.php
@@ -3,56 +3,56 @@ echo "<ul>";
 
 echo "<h3>User API</h3>";
 
-echo "<li><a href='harshil/ViewUsers.php'></a>View User</li>";
-echo"<li><a href='harshil/SignUp.php'></a>Sign Up</li>";
-echo"<li><a href='harshil/Profile.php'></a>Profile View</li>";
-echo"<li><a href='harshil/ProfileUpdate.php'></a>Profile Update</li>";
-echo"<li><a href='harshil/ProfileDelete.php'></a>Profile Delete</li>";
+echo "<li><a href='harshil/ViewUsers.php'>View User</a></li>";
+echo"<li><a href='harshil/SignUp.php'>Sign Up</a></li>";
+echo"<li><a href='harshil/Profile.php'>Profile View<</a>/li>";
+echo"<li><a href='harshil/ProfileUpdate.php'>Profile Update</a></li>";
+echo"<li><a href='harshil/ProfileDelete.php'>Profile Delete</a></li>";
 
 echo "<h3>Account API</h3>";
 
-echo"<li><a href='harshil/ViewAccounts.php'></a>View Accounts</li>";
-echo"<li><a href='harshil/AddAccount.php'></a>Add Account</li>";
-echo"<li><a href='harshil/UpdateAccount.php'></a>Update Account</li>";
-echo"<li><a href='harshil/DeleteAccount.php'></a>Delete Account</li>";
-echo"<li><a href='harshil/DeleteAccount.php'></a>Delete Account</li>";
+echo"<li><a href='harshil/ViewAccounts.php'>View Accounts</a></li>";
+echo"<li><a href='harshil/AddAccount.php'>Add Account</a></li>";
+echo"<li><a href='harshil/UpdateAccount.php'>Update Account</a></li>";
+echo"<li><a href='harshil/DeleteAccount.php'>Delete Account</a></li>";
+echo"<li><a href='harshil/DeleteAccount.php'>Delete Account</a></li>";
 
 echo "<h3>Transaction</h3>";
 
-echo "<li><a href='harshil/AddTransaction.php'></a>Add Transaction</li>";
-echo "<li><a href='harshil/ViewTransactions.php'></a>View Transactions</li>";
-echo "<li><a href='harshil/UpdateTransaction.php'></a>Update Transaction</li>";
-echo "<li><a href='harshil/DeleteTransaction.php'></a>Delete Transaction</li>";
-echo "<li><a href='harshil/SummaryReportTransaction.php'></a>Summary Report Transaction</li>";
+echo "<li><a href='harshil/AddTransaction.php'>Add Transaction</a></li>";
+echo "<li><a href='harshil/ViewTransactions.php'>View Transactions</a></li>";
+echo "<li><a href='harshil/UpdateTransaction.php'>Update Transaction</a></li>";
+echo "<li><a href='harshil/DeleteTransaction.php'>Delete Transaction</a></li>";
+echo "<li><a href='harshil/SummaryReportTransaction.php'>Summary Report Transaction</a></li>";
 
 echo "<h3>Category</h3>";
 
-echo "<li><a href='harshil/AddCategory.php'></a>Add Category</li>";
-echo "<li><a href='harshil/ViewCategory.php'></a>View Category</li>";
-echo "<li><a href='harshil/UpdateCategory.php'></a>Update Category</li>";
-echo "<li><a href='harshil/DeleteCategory.php'></a>Delete Category</li>";
+echo "<li><a href='harshil/AddCategory.php'>Add Category</a></li>";
+echo "<li><a href='harshil/ViewCategory.php'>View Category</a></li>";
+echo "<li><a href='harshil/UpdateCategory.php'>Update Category</a></li>";
+echo "<li><a href='harshil/DeleteCategory.php'>Delete Category</a></li>";
 
 echo "<h3>Expense</h3>";
 
-echo "<li><a href='harshil/AddExpense.php'></a>Add Expense</li>";
-echo "<li><a href='harshil/ViewExpenses.php'></a>View Expenses</li>";
-echo "<li><a href='harshil/DeleteExpense.php'></a>Delete Expense</li>";
-echo "<li><a href='harshil/UpdateExpense.php'></a>Update Expense</li>";
-echo "<li><a href='harshil/ViewExpenseById.php'></a>View Expense By Id</li>";
+echo "<li><a href='harshil/AddExpense.php'>Add Expense</a></li>";
+echo "<li><a href='harshil/ViewExpenses.php'>View Expenses</a></li>";
+echo "<li><a href='harshil/DeleteExpense.php'>Delete Expense</a></li>";
+echo "<li><a href='harshil/UpdateExpense.php'>Update Expense</a></li>";
+echo "<li><a href='harshil/ViewExpenseById.php'>View Expense By Id</a></li>";
 
 
 echo "<h3>Income</h3>";
 
-echo "<li><a href='harshil/AddIncome.php'></a>Add Income</li>";
-echo "<li><a href='harshil/ViewIncome.php'></a>View Income</li>";
-echo "<li><a href='harshil/UpdateIncome.php'></a>Update Income</li>";
-echo "<li><a href='harshil/DeleteIncome.php'></a>Delete Income</li>";
-echo "<li><a href='harshil/ViewIncomeById.php'></a>View Income By Id</li>";
+echo "<li><a href='harshil/AddIncome.php'>Add Income</a></li>";
+echo "<li><a href='harshil/ViewIncome.php'>View Income</a></li>";
+echo "<li><a href='harshil/UpdateIncome.php'>Update Income</a></li>";
+echo "<li><a href='harshil/DeleteIncome.php'>Delete Income</a></li>";
+echo "<li><a href='harshil/ViewIncomeById.php'>View Income By Id</a></li>";
 
 
 echo "<h3>Total</h3>";
 
-echo "<li><a href='harshil/SummaryReport.php'></a>Summary Report</li>";
+echo "<li><a href='harshil/SummaryReport.php'>Summary Report</a></li>";
 
 
 echo "</ul>";
