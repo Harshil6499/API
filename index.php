e9459c1efaae63568de8b632ddc18f4c03251e73
Harshil Shishangiya <harshilshishangiya777@gmail.com>
2025-11-19 00:53:07 +0530

    Update index.php
    some changes

--- index.php
+++ index.php
@@ -5,16 +5,17 @@ echo "<h3>User API</h3>";
 
 echo "<li><a href='harshil/ViewUsers.php'>View User</a></li>";
 echo"<li><a href='harshil/SignUp.php'>Sign Up</a></li>";
-echo"<li><a href='harshil/Profile.php'>Profile View<</a>/li>";
+echo"<li><a href='harshil/SignIn.php'>Sign In</a></li>";
+echo"<li><a href='harshil/Profile.php'>Profile View<</a></li>";
 echo"<li><a href='harshil/ProfileUpdate.php'>Profile Update</a></li>";
 echo"<li><a href='harshil/ProfileDelete.php'>Profile Delete</a></li>";
+echo"<li><a href='harshil/ChangePassword.php'>Change Password</a></li>";
 
 echo "<h3>Account API</h3>";
 
 echo"<li><a href='harshil/ViewAccounts.php'>View Accounts</a></li>";
 echo"<li><a href='harshil/AddAccount.php'>Add Account</a></li>";
 echo"<li><a href='harshil/UpdateAccount.php'>Update Account</a></li>";
-echo"<li><a href='harshil/DeleteAccount.php'>Delete Account</a></li>";
 echo"<li><a href='harshil/DeleteAccount.php'>Delete Account</a></li>";
 
 echo "<h3>Transaction</h3>";
@@ -50,9 +51,11 @@ echo "<li><a href='harshil/DeleteIncome.php'>Delete Income</a></li>";
 echo "<li><a href='harshil/ViewIncomeById.php'>View Income By Id</a></li>";
 
 
-echo "<h3>Total</h3>";
+echo "<h3>Extra</h3>";
 
 echo "<li><a href='harshil/SummaryReport.php'>Summary Report</a></li>";
+echo "<li><a href='harshil/SummaryReportTransaction.php'>Summary Report Transaction</a></li>";
+echo "<li><a href='harshil/MonthlySummary.php'>Monthly Summary</a></li>";
 
 
 echo "</ul>";
